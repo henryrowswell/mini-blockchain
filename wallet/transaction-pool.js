1a146a5ea3374b24cfabe011c9eee4ee4523c7a7
Henry Rowswell <hrowswell@gofundme.com>
2021-02-20 23:06:13 -0500

    part 2: wallets, transactions, transaction pools, and p2p network

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,22 @@
+const Transaction = require("./transaction")
+
+class TransactionPool {
+    constructor() {
+        this.transactions = []
+    }
+
+    updateOrAddTransaction(transaction) {
+        let transactionWithId = this.transactions.find(t => t.id === transaction.id)
+        if (transactionWithId !== undefined) {
+            this.transactions[this.transactions.indexOf(transactionWithId)] = transaction
+        } else {
+            this.transactions.push(transaction)
+        }
+    }
+
+    existingTransaction(address) {
+        return this.transactions.find(t => t.input.address === address)
+    }
+}
+
+module.exports = TransactionPool
