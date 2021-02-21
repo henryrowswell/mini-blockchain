763ebfe68410dba35c51c2372a5aa3ca7d6817a9
Henry Rowswell <hrowswell@gofundme.com>
2021-02-21 00:35:37 -0500

    part 3: mine transactions and get rewards

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const Blockchain = require('../blockchain')
 const P2pServer = require('./p2p-server')
 const Wallet = require('../wallet')
 const TransactionPool = require('../wallet/transaction-pool')
+const Miner = require('./miner')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
@@ -13,6 +14,7 @@ const bc = new Blockchain()
 const wallet = new Wallet()
 const tp = new TransactionPool()
 const p2pServer = new P2pServer(bc, tp)
+const miner = new Miner(bc, tp, wallet, p2pServer)
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain)
@@ -31,10 +33,16 @@ app.get('/transactions', (req, res) => {
 
 app.post('/transact', (req, res) => {
     const { recipient, amount } = req.body
-    const transaction = wallet.createTransaction(recipient, amount, tp)
+    const transaction = wallet.createTransaction(recipient, amount, bc, tp)
     p2pServer.broadcastTransaction(transaction)
 
     res.redirect('/transactions')
+})
+
+app.get('/mine-transactions', (req, res) => {
+    const block = miner.mine()
+    console.log(`New block added: ${block.toString()}`)
+    res.redirect('/blocks')
 })
 
 app.get('/public-key', (req, res) => {
